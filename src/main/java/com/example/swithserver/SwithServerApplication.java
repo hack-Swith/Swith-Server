34076ca6f7b7a20df880f7cda1ec177f4f74e04c
seongwon <seongwon1290@dsm.hs.kr>
2023-07-04 17:23:15 +0900

    📁 :: 프로젝트 생성

---
+++ src/main/java/com/example/swithserver/SwithServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.swithserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwithServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwithServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swithserver/SwithServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swithserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwithServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
